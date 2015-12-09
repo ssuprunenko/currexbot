f61f37382489e951ed12825199c2a7ede58efa1c
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-12-09 19:00:47 +0300

    [add] Handle simple commands

---
+++ lib/currexbot/bot.ex
@@ -0,0 +1,26 @@
+defmodule Currexbot.Bot do
+  alias Nadia.Model.Message
+  alias Nadia.Model.Chat
+  alias Nadia.Model.User
+
+  @doc """
+  handle incoming message
+  """
+  def handle_message(%Message{chat: %Chat{type: "private", id: chat_id}, text: text}) do
+    handle_private_message(chat_id, text)
+  end
+
+  # fallback
+  def handle_message(_), do: true
+
+  defp handle_private_message(chat_id, "hello") do
+    Nadia.send_message(chat_id, "yo")
+  end
+
+  defp handle_private_message(chat_id, "/me") do
+    {:ok, %User{first_name: bot_name}} = Nadia.get_me
+    Nadia.send_message(chat_id, bot_name)
+  end
+
+  defp handle_private_message(_chat_id, _), do: true
+end
