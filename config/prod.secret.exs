dd49ed92789bdb31936573f3ee07009e0f50896e
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-12-26 18:43:59 +0300

    Configure postgres on production server

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -2,3 +2,8 @@ use Mix.Config
 
 config :nadia,
   token: System.get_env("TELEGRAM_BOT_TOKEN")
+
+config :currexbot, Currexbot.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
