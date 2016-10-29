8125ab39071d7572512570951ddbe8ded6a171f1
Sergey Suprunenko <suprunenko.s@gmail.com>
2016-10-29 19:19:11 +0300

    Upgrade Elixir to 1.3.4

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Currexbot.Mixfile do
 
   def project do
     [app: :currexbot,
-     version: "0.1.0",
-     elixir: "~> 1.2",
+     version: "0.2.0",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
